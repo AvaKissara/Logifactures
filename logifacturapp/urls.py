147e450b15fdb1e8f6a53be12bf3687aeede7dc2
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2024-02-20 02:18:07 +0100

    Restructuration du html

--- logifacturapp/urls.py
+++ logifacturapp/urls.py
@@ -5,7 +5,6 @@ from .views import IndexView, ListeFactureView, ListeFournisseurView, DetailView
 app_name = "logifacturapp"
 
 urlpatterns = [
-    # path("<int:devise_id>/", views.detail, name="detail"),
     path('import-facture/', ImportFactureView.as_view(), name='import_facture'),
     path("<int:pk>/", DetailView.as_view(), name="detail"),
     path('', IndexView.as_view(), name='index'),
