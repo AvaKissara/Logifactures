5e368eac418cfe6ed35fe9472218667650654be0
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2024-03-04 16:39:03 +0100

    Ajout popup detail OK

--- logifacturapp/urls.py
+++ logifacturapp/urls.py
@@ -6,7 +6,7 @@ app_name = "logifacturapp"
 
 urlpatterns = [
     path('import-facture/', ImportFactureView.as_view(), name='import_facture'),
-    path("<int:pk>/", DetailView.as_view(), name="detail"),
+    path("facture/liste_facture/<int:pk>/", DetailView.as_view(), name="detail"),
     path('', IndexView.as_view(), name='index'),
     path('facture/delete/<int:facture_id>/', DeleteFactureView.as_view(), name='delete_facture'),
     path('facture/liste_facture/', ListeFactureView.as_view(), name='liste_facture'),
--- logifacturapp/views/detail_view.py
+++ logifacturapp/views/detail_view.py
@@ -1,8 +1,24 @@
 from django.views import View
 from django.views import generic
 from . .models import Facture
+from django.http import JsonResponse
 
 class DetailView(generic.DetailView):
     model = Facture
-    template_name = "logifacturapp/detail.html"
     context_object_name = "facture"
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        facture = Facture.objects.get(pk=pk)
+        data = {
+            'num_facture' : facture.num_facture,
+            'date_facture': facture.date_facture.strftime("%d-%m-%Y"),
+            'nom_client': f"{facture.client.nom_client} {facture.client.prenom_client}",
+            'r_social_fournisseur': facture.fournisseur.r_social_fourn,
+            'total_ht': f"{facture.total_ht_facture} {facture.devise.symb_devise}",
+            'total_ttc': f"{facture.total_ttc_facture} {facture.devise.symb_devise}",
+            'methode_paiement': facture.methode_paiement.nom_m_paie,
+            'statut_facture': facture.statut_facture,
+            'cat_facture': facture.cat_facture.nom_cat_facture,
+        }
+        return JsonResponse(data)
