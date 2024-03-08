c654968c86cdc27832cbc4bb67a9d97f9435863a
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2024-03-08 21:13:30 +0100

    sql maj OK

---
+++ Devise.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 08 mars 2024 à 20:12
+-- Version du serveur : 8.0.27
+-- Version de PHP : 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `logifacture`
+--
+
+-- --------------------------------------------------------
+
+--
+INSERT INTO `Devise` (`id_devise`, `nom_devise`, `abbr_devise`, `symb_devise`) VALUES
+(1, 'euro', 'eur', '€'),
+(2, 'dollar américain', 'usd', '$'),
+(3, 'livre sterling', 'gpb', '£');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
