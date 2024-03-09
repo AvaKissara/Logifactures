77686c66afdec09190fba02868c3a978055cebfa
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2024-03-09 11:39:23 +0100

    gitignore maj OK

--- logifacturapp/apps.py
+++ logifacturapp/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
-from django.core.wsgi import get_wsgi_application
 
 
 class logifacturappConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'logifacturapp'
-
-    def get_wsgi_application(self):
-        return get_wsgi_application()
